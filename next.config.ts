501866350a3fe9393db3f13d340416e56e7c0f11
Keerthu7 <keerthanasubbu5@gmail.com>
2026-08-27 16:35:18 +0530

    url changes for android and ios

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/services/mobile-applications',
+        destination: '/services/mobile-apps',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
